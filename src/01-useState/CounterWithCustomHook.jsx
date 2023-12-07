71605fc09691ff78de6eeed5d05cc981d7a8c886
Overdmg <127998833+Overdmg@users.noreply.github.com>
2023-12-07 12:15:25 -0400

    customH
    prueba de custom hook

---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,13 @@
+
+export const CounterWithCustomHook = () => {
+
+    return (
+        <>
+            <h1> Counter with Hook: DATA AQUI </h1>
+            <hr/>
+            <button className = "btn btn-primary" >+1</button>
+            <button className = "btn btn-warning" >Reset</button>
+            <button className = "btn btn-danger" >-1</button>
+        </>
+  )
+}
