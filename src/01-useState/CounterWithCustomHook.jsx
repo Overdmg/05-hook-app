a28f99728c0ddc964f030345ae2d12bbde62ecfa
Overdmg <127998833+Overdmg@users.noreply.github.com>
2023-12-12 14:47:59 -0400

    update
    cambio 2

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+import { useCounter } from "../hooks/useCounter";
 
 export const CounterWithCustomHook = () => {
 
+  const { counter } = useCounter();
+
+
     return (
         <>
-            <h1> Counter with Hook: DATA AQUI </h1>
+            <h1> Counter with Hook: { counter } </h1>
             <hr/>
-            <button className = "btn btn-primary" >+1</button>
-            <button className = "btn btn-warning" >Reset</button>
-            <button className = "btn btn-danger" >-1</button>
+            <button 
+                className = "btn btn-primary" 
+                onClick={ 
+                    () => (increment)
+                }
+            >+1</button>
+            <button 
+                className = "btn btn-warning" 
+
+            >Reset</button>
+            <button 
+                className = "btn btn-danger" 
+            
+            >-1</button>
         </>
   )
 }
