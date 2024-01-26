30ac02228fe57233c9fedfd9161152eeed6b2582
Overdmg <127998833+Overdmg@users.noreply.github.com>
2024-01-26 10:22:44 -0400

    Update SimpleForm.jsx
    avance del curso

--- src/02-useEffect/SimpleForm.jsx
+++ src/02-useEffect/SimpleForm.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
 
 export const SimpleForm = () => {
+
+const [formState, setFormState] = useState({
+    username: 'Overdmg',
+    email: 'overdmg@correo.com.do'
+
+});
+
+  const {username, email} = formState;
+
+  const onImputChange = ({ target }) => {
+      const { name, value } = target;
+      setFormState({
+        ...formState,
+        [name]: value
+      })
+
+  }
 
   return (
     <>
@@ -11,17 +29,18 @@ export const SimpleForm = () => {
             className="form-control"
             placeholder="Favor colocar el usuario"
             name="username"
-        />
+            value={ username }
+            onChange={ onImputChange }
+    />
 
         <input
             type="email"
             className="form-control mt-2"
             placeholder="Favor colocar el email"
             name="email"
+            value={ email }
+            onChange={ onImputChange }
         />
-
-
-
     </>
   )
 }
