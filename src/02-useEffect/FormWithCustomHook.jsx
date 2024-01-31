1e0b3de6d86caaf7a06eafcaf14b70f5e585b801
Overdmg <127998833+Overdmg@users.noreply.github.com>
2024-01-31 16:33:35 -0400

    update form
    se crea form con password

---
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import { useForm } from "../hooks/useForm";
+
+
+export const FormWithCustomHook = () => {
+
+    const {formState, onImputChange, username, password, email} = useForm({
+      username: '',
+      password: '',
+      email: ''
+
+    });
+
+    //const { username, password, email } = formState;
+
+  return (
+    <>
+        <h1>Formulario con Custom Hook</h1>
+        <hr />
+
+        <input
+            type="text"
+            className="form-control"
+            placeholder="Favor colocar el usuario"
+            name="username"
+            value={ username }
+            onChange={ onImputChange }
+    />
+
+        <input
+            type="password"
+            className="form-control mt-2"
+            placeholder="Contraseña"
+            name="password"
+            value={ password }
+            onChange={ onImputChange }
+        />
+
+        <input
+            type="email"
+            className="form-control mt-2"
+            placeholder="Favor colocar el email"
+            name="email"
+            value={ email }
+            onChange={ onImputChange }
+        />      
+
+
+    </>
+  )
+}
