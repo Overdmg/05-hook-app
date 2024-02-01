1ee94226417661b0d01e9014c9c005a7d061edb3
Overdmg <127998833+Overdmg@users.noreply.github.com>
2024-02-01 16:53:26 -0400

    Update MultipleCustomHooks.jsx
    update

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -6,13 +6,32 @@ export const MultipleCustomHooks = () => {
 
     const { data, isLoading, hasError} = useFetch('https://api.breakingbadquotes.xyz/v1/quotes');
 
+    const {quote, author} = !!data && data[0];
+
     console.log(data, isLoading, hasError);
+
 
   return ( 
     <>
         <h1>BreakingBad Quotes</h1>
         <hr />    
-    
+
+        {
+            isLoading
+                ? (
+                    <div className='alert alert-info text-center'>
+                        Cargando...
+                    </div>
+               )
+                : (
+                    <blockquote className='blockquote text-end'>
+                        <p className='mb-2'>{quote}</p>
+                        <footer className='blockquote-footer'>{author}</footer>
+                    </blockquote>
+             )
+
+        }
+
     </>
   )
 }
