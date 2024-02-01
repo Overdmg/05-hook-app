d4c7fb0ba69104e9b6d0b309d7ee18043553c3e6
Overdmg <127998833+Overdmg@users.noreply.github.com>
2024-02-01 16:00:16 -0400

    update fetch
    se completo el req para el fetch

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { useFetch } from '../hooks/useFetch';
+
 
 export const MultipleCustomHooks = () => {
 
-    
-  return (
+    const { data, isLoading, hasError} = useFetch('https://api.breakingbadquotes.xyz/v1/quotes');
+
+    console.log(data, isLoading, hasError);
+
+  return ( 
     <>
         <h1>BreakingBad Quotes</h1>
         <hr />    
