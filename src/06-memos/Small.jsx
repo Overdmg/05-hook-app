6b6bf1697ce38940fb6b6d0da07c45e0b45ae1e4
Overdmg <127998833+Overdmg@users.noreply.github.com>
2024-02-07 11:31:59 -0400

    useMemo
    update 1

---
+++ src/06-memos/Small.jsx
@@ -0,0 +1,11 @@
+
+
+export const Small = ({ value }) => {
+  
+  
+    return (
+    
+        <small> {value} </small>
+
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,12 @@ import './index.css'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 //import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-UseLayouEffect/Layout'
+//import { Layout } from './05-UseLayouEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <Layout/>
+    <Memorize/>
   //</React.StrictMode>,
 )
