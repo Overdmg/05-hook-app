8a23d068f26d05a7118893a1b0642aba4031b05b
Overdmg <127998833+Overdmg@users.noreply.github.com>
2024-02-28 15:00:11 -0400

    Update intro-reducer.js
    upd

--- src/08-useReducer/intro-reducer.js
+++ src/08-useReducer/intro-reducer.js
@@ -1,18 +1,37 @@
+
+
 const initialState = [{
-    id:1,
+    id: 1,
     todo: 'Practicar Next',
     done: false,
 
 }];
 
 
-const todoReducer = ( state = initialState, action ) => {
+const todoReducer = ( state = initialState, action = {} ) => {
 
+    if ( action.type === '[TODO] Add todo'){
+            return [ ...state, action.payload ];
+    }
 
     return state;
 };
 
 
 let todos = todoReducer();
+
+const newTodo = {
+    id: 2,
+    todo: 'Avanzar en Next',
+    done: false
+}
+
+const addTodoAction = {
+        type: '[TODO] Add todo',
+        payload: newTodo
+}
+
+todos = todoReducer( todos, addTodoAction)
+
 
 console.log(todos);
